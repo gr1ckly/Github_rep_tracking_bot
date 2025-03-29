adeca2d74fb38c8db3e89fcb7e13b09400654cfe
gr1ckly <yarm445@gmail.com>
2025-03-30 01:29:27 +0300

    refactor: use common ChangingDTO instead separate DTOS

--- MainServer/LinkTracker/dtos/link_tracker_dtos.go
+++ MainServer/LinkTracker/dtos/link_tracker_dtos.go
@@ -2,29 +2,11 @@ package dtos
 
 import "time"
 
-type CommitChangesDTO struct {
-	ChatId   int    `json:"chat_id"`
-	Link     string `json:"link"`
-	Author   string `json:"author"`
-	Commiter string `json:"commiter"`
-	Message  string `json:"message"`
-	Branch   string `json:"branch"`
-}
-
-type IssueChangesSTO struct {
+type ChangingDTO struct {
 	ChatId    int       `json:"chat_id"`
 	Link      string    `json:"link"`
+	Event     string    `json:"event"`
 	Author    string    `json:"author"`
 	Title     string    `json:"title"`
-	UpdatedAt time.Time `json:"updated_at"`
-}
-
-type PullRequestChangesDTO struct {
-	ChatId    int       `json:"chat_id"`
-	Link      string    `json:"link"`
-	Author    string    `json:"author"`
-	Number    int       `json:"number"`
-	Title     string    `json:"title"`
-	Status    string    `json:"status"`
 	UpdatedAt time.Time `json:"updated_at"`
 }
